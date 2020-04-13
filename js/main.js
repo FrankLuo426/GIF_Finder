a4590d34ce61ab28dedddc748434f76d75e0dfe6
FrankLuo426 <caesarfrank@hotmail.com>
2020-04-13 13:51:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function bgChange(thisColor) {
 }
 
 //create a GIF search buttton
-function GIFsearchButtonClicked(){
+function GIFsearchButtonClicked() {
     //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
     const GIF_Search_URL = "https://api.giphy.com/v1/gifs/search?";
     //url to store the value
@@ -39,52 +39,52 @@ function GIFsearchButtonClicked(){
 
 }
 
-function STICKERsearchButtonClicked(){
-        //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
-        const STICKER_Search_URL = "https://api.giphy.com/v1/stickers/search?";
-        //url to store the value
-        //require api_key, q, limit, offset, rating, lang,  random_id.
-        //the first one is the GIF_URL
-        let url = STICKER_Search_URL;
-        //decleasr some required value
-        let api_key = "tH7NkuJUOcVVGQ7yoFMoUkTgppTJUEY9";
-        //parse in the api_key
-        url += "api_key=" + api_key;
-        //get and parse the search item
-        let item = document.querySelector("#searchSTICKER").value;
-        displayTerm = item;
-        //fix the search item problem
-        item = item.trim();
-        item = encodeURIComponent(item);
-        //make sure the item greater than 1
-        if (item.length < 1) {
-            return;
-        }
-        //parse in item
-        url += "&q=" + item;
-    
-        //set and parse in the limit 
-        let limit = document.querySelector("#limitSTICKER").value;
-        url += "&limit=" + limit;
-    
-        //update
-        document.querySelector("#status").innerHTML = "<b>Searching For STICKER: '" + displayTerm + "'</b>";
+function STICKERsearchButtonClicked() {
+    //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
+    const STICKER_Search_URL = "https://api.giphy.com/v1/stickers/search?";
+    //url to store the value
+    //require api_key, q, limit, offset, rating, lang,  random_id.
+    //the first one is the GIF_URL
+    let url = STICKER_Search_URL;
+    //decleasr some required value
+    let api_key = "tH7NkuJUOcVVGQ7yoFMoUkTgppTJUEY9";
+    //parse in the api_key
+    url += "api_key=" + api_key;
+    //get and parse the search item
+    let item = document.querySelector("#searchSTICKER").value;
+    displayTerm = item;
+    //fix the search item problem
+    item = item.trim();
+    item = encodeURIComponent(item);
+    //make sure the item greater than 1
+    if (item.length < 1) {
+        return;
+    }
+    //parse in item
+    url += "&q=" + item;
 
-        getData(url);
+    //set and parse in the limit 
+    let limit = document.querySelector("#limitSTICKER").value;
+    url += "&limit=" + limit;
+
+    //update
+    document.querySelector("#status").innerHTML = "<b>Searching For STICKER: '" + displayTerm + "'</b>";
+
+    getData(url);
 }
 
 
 
 //Downloading the data with XHR
-function getData(url){
+function getData(url) {
     let xhr = new XMLHttpRequest();
     xhr.onload = dataLoaded;
     xhr.onerror = dataError;
-    xhr.open("GET",url);
+    xhr.open("GET", url);
     xhr.send();
 }
 //Create the callback functions
-function dataLoaded(e){
+function dataLoaded(e) {
     let xhr = e.target;
 
     //puty texxt into an obj
@@ -110,6 +110,7 @@ function dataLoaded(e){
         var line = `<div class='result'><a target='_blank' href='${url}'><img src='${smallURL}' title= '${result.id}'/></a>`;
         line += `<p>Rating: ${rating}</p>`;
         line += `<button class="copyButton" onclick="copyText('${url}')">Click here to copy URL</button></div>`;
+        line += `<button class="copyButton" onclick="moreinfo('${result}')">More INFO</button></div>`;
         bigString += line;
     }
 
@@ -121,24 +122,27 @@ function dataLoaded(e){
 }
 
 //copy url
-function copyText(url){
-const input = document.createElement('input');
-input.setAttribute('readonly', 'readonly'); 
-input.setAttribute('value', url);
-document.body.appendChild(input);
-input.select();
-input.setSelectionRange(0, 9999);
-if(document.execCommand("Copy","false","null"))
-{
-    alert("Copy Success!");
-}
-else
-{
-    alert("Copy Failed...")
-}
+function copyText(url) {
+    const input = document.createElement('input');
+    input.setAttribute('readonly', 'readonly');
+    input.setAttribute('value', url);
+    document.body.appendChild(input);
+    input.select();
+    input.setSelectionRange(0, 9999);
+    if (document.execCommand("Copy", "false", "null")) {
+        alert("Copy Success!");
+    } else {
+        alert("Copy Failed...")
+    }
 
 }
 
-function dataError(e){
+
+//more info
+function moreinfo(result) {
+    
+}
+
+function dataError(e) {
 
 }
