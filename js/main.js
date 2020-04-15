59e761b22b7a4305a7f27e060f1cc959380dcca1
FrankLuo426 <caesarfrank@hotmail.com>
2020-04-15 16:53:14 -0400

    做了一个新的返回上部的按钮，做了一个缩小后减少每行数量的responsive

--- js/main.js
+++ js/main.js
@@ -214,3 +214,25 @@ function ClearButtonClicked(){
     document.querySelector("#content").innerHTML = "";
     document.querySelector(".contentP").innerHTML = "No data yet!";
 }
+
+
+
+//Get the button:
+ let mybutton = document.querySelector("#myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
