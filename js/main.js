5dcbd03a947607ba99e96d0fb6dafaaf373f5f63
John Zhang <35859541+XiuLan123@users.noreply.github.com>
2020-04-16 05:29:46 +0800

    fix console error

--- js/main.js
+++ js/main.js
@@ -218,21 +218,8 @@ function ClearButtonClicked(){
     document.querySelector(".contentP").innerHTML = "All results have cleared!";
 }
 
-
-
 //Get the button:
  let mybutton = document.querySelector("#myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
